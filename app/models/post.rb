c7a4a06824a13002b45337a5a21369f2ad7128f6
jkral <jeff@sandboxbrands.com>
2016-01-15 13:41:46 -0500

    s3 out of model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,9 @@ class Post < ActiveRecord::Base
   validates :image, presence: true
 
   has_attached_file :image, styles: { :medium => "640x" },
-  									:storage => :s3,
-  									:bucket => 'photobrag',
-  									:s3_credentials => S3_CREDENTIALS
+  									# :storage => :s3,
+  									# :bucket => 'photobrag',
+  									# :s3_credentials => S3_CREDENTIALS
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end 
 
